1fe322ccf61f06afa5de7addb1b1d8274d95e67b
Alan Smith <alan@alanwsmith.com>
2022-02-09 17:34:18 -0500

    Basic code highlighting in place

--- site/components/Renderer.js
+++ site/components/Renderer.js
@@ -1,23 +1,80 @@
 import Script from 'next/script'
+import React from 'react'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import nightOwl from 'prism-react-renderer/themes/nightOwl'
+import dracula from 'prism-react-renderer/themes/dracula'
 
-export default function Renderer({ theCode }) {
+export default function Renderer({ code, langauge }) {
   const regex = /console.log\((.*?)\)/
-  const match = theCode.match(regex)
+  const match = code.match(regex)
   const element_id = `id_${match[1]}`
-  const runCode = theCode.replace(
+  const runCode = code.replace(
     regex,
     `document.getElementById('${element_id}').innerText = $1`
   )
 
+  // <pre className={`${className} code_block`} style={style}>
+  //
   return (
     <>
-      <pre>
-        <code>{theCode.trim()}</code>
+      <Highlight
+        {...defaultProps}
+        theme={dracula}
+        code={code.trim()}
+        language={'jsx'}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={`${className} code_block`} style={style}>
+            <button
+              onClick={() => {
+                const code_text = document.getElementById(`${element_id}_code`)
+                code_text.select()
+                document.execCommand('copy')
+                console.log('hasdf')
+              }}
+              style={{ position: 'absolute', top: 0, right: 0, opacity: 0.4 }}
+            >
+              Copy
+            </button>
+            {tokens.map((line, i) => (
+              <div
+                key={i}
+                {...getLineProps({ line, key: i })}
+                className="token-line code_line"
+              >
+                <span
+                  className={
+                    i === 0
+                      ? `code_line_number code_line_number_first`
+                      : i === tokens.length - 1
+                      ? `code_line_number code_line_number_last`
+                      : `code_line_number`
+                  }
+                >
+                  {i + 1}
+                </span>
+                <span className={`code_line_content`}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </span>
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+      <textarea
+        id={`${element_id}_code`}
+        value={code.trim()}
+        readOnly={true}
+        style={{ position: 'absolute', left: '-9000px' }}
+      ></textarea>
+      <p>Example Output:</p>
+
+      <pre className="code_block">
+        <code className="code_lines" id={element_id}></code>
       </pre>
-      <p>Outputs:</p>
-      <pre>
-        <code id={element_id}></code>
-      </pre>
+
       <Script
         id={`${element_id}_script`}
         strategy="afterInteractive"
--- site/pages/random-numbers/index.js
+++ site/pages/random-numbers/index.js
@@ -3,18 +3,24 @@ import Renderer from '../../components/Renderer'
 export default function Page(props) {
   const basic_example = `
 const basic_example = Math.random()
+
 console.log(basic_example)
 `
 
   const up_to_number = `
 const max_number = 10
 const up_to_number = Math.floor((Math.random() * max_number) + 1)
+
 console.log(up_to_number)
 `
   const between_nums = `
 const min = 4
 const max = 20
-const between_nums = Math.floor((Math.random() * (max - min + 1)) + min)
+const between_nums = Math.floor(
+  (Math.random() * (max - min + 1)) 
+  + min
+)
+
 console.log(between_nums)
 `
 
@@ -23,20 +29,20 @@ console.log(between_nums)
       <h1>Random Numbers</h1>
       <h2>Basic Call</h2>
       <p>This one returns a random number between 0 and 1.</p>
-      <Renderer theCode={basic_example} />
+      <Renderer code={basic_example} language="javascript" />
 
       <h2>Up To Max</h2>
       <p>
         This will generate a random number between 1 and the max number given.
         (Including the possiblity of both 1 and the max number being picked.)
       </p>
-      <Renderer theCode={up_to_number} />
+      <Renderer code={up_to_number} language="javascript" />
 
       <h2>Between Two Numbers</h2>
       <p>
         This pulls in between two numbers (inclucing the numbers themselves).
       </p>
-      <Renderer theCode={between_nums} />
+      <Renderer code={between_nums} language="javascript" />
     </>
   )
 }
