2a66099fa6e700a9a733894acd972473a3388593
Alan Smith <alan@alanwsmith.com>
2022-02-08 23:46:32 -0500

    Moving things into a single file for the random numbers

--- site/pages/random-numbers/index.js
+++ site/pages/random-numbers/index.js
@@ -1,44 +1,69 @@
-import fs from 'fs'
 import Script from 'next/script'
 
 export default function Page(props) {
+  const basic_example = `
+const basic_example = Math.random()
+document.getElementById('basic_example').innerText = basic_example
+`
+
+  const up_to_number = `
+const max_number = 10
+const up_to_number = Math.floor((Math.random() * max_number) + 1)
+document.getElementById('up_to_number').innerText = up_to_number
+`
+  const between_nums = `
+const min = 4
+const max = 20
+const between_nums = Math.floor((Math.random() * (max - min + 1)) + min)
+document.getElementById('between_nums').innerText = between_nums
+  `
+
   return (
     <>
-      <p>
-        Here&apos;s the basic call which returns a number between zero and one
-        (without ever ending up at either zero or one directly)
-      </p>
+      <h1>Random Numbers</h1>
+
+      <h2>Basic Call</h2>
+      <p>This is a baisc call to Math.random()</p>
       <pre>
-        <code>{props.random_number_script}</code>
+        <code>{basic_example.trim()}</code>
       </pre>
-      <div id="basic"></div>
+      <p>Example Output:</p>
+      <div id="basic_example"></div>
       <Script
-        id="basicRandomNumberScript"
+        id="basic_example_script"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: props.random_number_script,
+          __html: basic_example,
+        }}
+      />
+
+      <h2>Between 1 And A Max Number</h2>
+      <pre>
+        <code>{up_to_number.trim()}</code>
+      </pre>
+      <p>Example Output:</p>
+      <div id="up_to_number"></div>
+      <Script
+        id="up_to_number_script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: up_to_number,
+        }}
+      />
+
+      <h2>Between Two Numbers (with min and max)</h2>
+      <pre>
+        <code>{between_nums.trim()}</code>
+      </pre>
+      <p>Example Output:</p>
+      <div id="between_nums"></div>
+      <Script
+        id="between_nums_script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: between_nums,
         }}
       />
     </>
   )
-}
-
-export async function getStaticProps(context) {
-  // Helper function for loading files
-  function loadFileAsString(path) {
-    try {
-      const fileText = fs.readFileSync(path, `utf8`)
-      return fileText
-    } catch (err) {
-      return `Could not load: ${path}`
-    }
-  }
-  const random_number_script = loadFileAsString(
-    `./pages/random-numbers/random-number.script`
-  )
-  return {
-    props: {
-      random_number_script: random_number_script,
-    },
-  }
 }
