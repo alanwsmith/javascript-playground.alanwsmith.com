5215ebfe3797a24cb46572bb2a287b39f756ebd7
Alan Smith <alan@alanwsmith.com>
2022-02-09 18:09:00 -0500

    Reordered random examples

--- site/pages/random-numbers/index.js
+++ site/pages/random-numbers/index.js
@@ -27,14 +27,11 @@ console.log(between_nums)
   return (
     <>
       <h1>Random Numbers</h1>
-      <h2>Basic Call</h2>
-      <p>This one returns a random number between 0 and 1.</p>
-      <Renderer code={basic_example} language="javascript" />
-
-      <h2>Up To Max</h2>
+      <h2>Between 1 And A Max Number</h2>
       <p>
-        This will generate a random number between 1 and the max number given.
-        (Including the possiblity of both 1 and the max number being picked.)
+        This is the one I use the most. It generates a random number between 1
+        and the max number given. (Including the possiblity of both 1 and the
+        max number being picked.)
       </p>
       <Renderer code={up_to_number} language="javascript" />
 
@@ -43,6 +40,13 @@ console.log(between_nums)
         This pulls in between two numbers (inclucing the numbers themselves).
       </p>
       <Renderer code={between_nums} language="javascript" />
+
+      <h2>Basic Call</h2>
+      <p>
+        This is the basic functionality which provides a decimal number between
+        0 and 1. 
+      </p>
+      <Renderer code={basic_example} language="javascript" />
     </>
   )
 }
