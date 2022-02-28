3c4682aa507962c07cf949a872b31be12b626e2f
Alan Smith <alan@alanwsmith.com>
2022-02-28 01:30:05 -0500

    Updated display to show dots and move the output in the same box

--- site/components/RendererV2.js
+++ site/components/RendererV2.js
@@ -3,39 +3,6 @@ import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import nightOwl from 'prism-react-renderer/themes/nightOwl'
 
-// const holding = `
-//               <button
-//                 onClick={() => {
-//                   const code_text = document.getElementById(
-//                     `${element_id}_code`
-//                   )
-//                   code_text.select()
-//                   document.execCommand('copy')
-//                   console.log('hasdf')
-//                 }}
-//                 style={{ position: 'absolute', top: 0, right: 0, opacity: 0.4 }}
-//               >
-//                 Copy
-//               </button>
-//             <p>Example Output:</p>
-//             <pre className="code_block_output" style={style}>
-//               <code className="code_lines" id={element_id}></code>
-//             </pre>
-//       <textarea
-//         id={`${element_id}_code`}
-//         value={code.trim()}
-//         readOnly={true}
-//         style={{ position: 'absolute', left: '-9000px' }}
-//       ></textarea>
-//       <Script
-//         id={`${element_id}_script`}
-//         strategy="afterInteractive"
-//         dangerouslySetInnerHTML={{
-//           __html: runCode,
-//         }}
-//       />
-// `
-
 export default function RendererV2({ example, language }) {
   const snippet_id = `snippet_${example.output}`
   const runCode = `
@@ -127,8 +94,11 @@ document.getElementById('${snippet_id}').innerText = ${example.output}`
                   {...getLineProps({ line, key: i })}
                   className="token-line code_line"
                 >
-                  <span className="code_line_number_blank">&nbsp;</span>
+                  <span className="code_line_number_blank">
+                    .<br />.
+                  </span>
                   <span className={`code_line_content`}>
+                    <br />
                     {line.map((token, key) => (
                       <span key={key} {...getTokenProps({ token, key })} />
                     ))}
